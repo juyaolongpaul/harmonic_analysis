3867d741430d7a5baf6b88a9afdc1d34eb4a892e
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2019-05-20 01:22:00 -0400

    WIP, enable the ML model to generate chord labels for other music

---
+++ predict_new_music.py
@@ -0,0 +1,185 @@
+from keras.models import load_model
+from transpose_to_C_polyphony import transpose_polyphony
+import os
+from music21 import *
+from get_input_and_output import *
+from predict_result_for_140 import get_predict_file_name
+
+def generate_ML_matrix(path, windowsize, sign='N'):
+    counter = 0
+    fn_all = []  # Unify the order
+    for fn in os.listdir(path):
+        if sign == 'N':  # eliminate pitch class only encoding
+            if fn.find('_pitch_class') != -1 or fn.find('_chord_tone') != -1:
+                continue
+        elif sign == 'Y':  # only want pitch class only encoding
+            if fn.find('_pitch_class') == -1:
+                continue
+        elif sign == 'C':  # only want chord tone as input to train the chord inferral algorithm
+            if fn.find('_chord_tone') == -1:
+                continue
+        if fn.find('CKE') == -1 and fn.find('C_oriKE') == -1 and fn.find('aKE') == -1 and fn.find(
+                'a_oriKE') == -1:  # we cannot find key of c, skip
+            continue
+        # elif portion == 'valid' or portion == 'test': # we want original key on valid and test set when augmenting
+        #     if fn.find('_ori') == -1:
+        #         continue
+        fn_all.append(fn)
+    print(fn_all)
+    fn_all.sort()
+    print(fn_all)
+    for fn in fn_all:
+        encoding = np.loadtxt(os.path.join(path, fn))
+        encoding_window = adding_window_one_hot(encoding, windowsize)
+        if counter == 0:
+            encoding_all = list(encoding_window)
+            encoding_all = np.array(encoding_all)
+        else:
+            encoding_all = np.concatenate((encoding_all, encoding_window))
+        counter += 1
+    print('finished')
+    return encoding_all
+
+
+def get_input_encoding(inputpath, encoding_path):
+    augmentation = 'N'
+    fn_total = []
+    if not os.path.isdir(encoding_path):
+        os.mkdir(encoding_path)
+
+    for id, fn in enumerate(os.listdir(
+            inputpath)):  # this part should be executed no matter what since we want a updated version of chord list
+        if fn.find('KB') != -1:
+            if fn.find('CKE') != -1 or fn.find('C_oriKE') != -1:
+                fn_total.append(fn)
+            if fn.find('aKE') != -1 or fn.find('a_oriKE') != -1:  # only wants key c
+                fn_total.append(fn)
+            else:
+                print('they are not qualified', fn)
+
+    for id, fn in enumerate(fn_total):
+        if len(os.listdir(encoding_path)) > 200: break
+        chorale_x = []
+        # pitch class is used. This one is used to indicate which one is NCT.
+        chorale_x_only_pitch_class = []
+        chorale_x_only_meter = []  # we want to save the meter info for the gt chord label as input feature to do chord inferral
+        chorale_x_only_newOnset = []
+        print(fn, id)
+        s = converter.parse(os.path.join(inputpath, fn))
+        sChords = s.chordify(removeRedundantPitches=False)
+        part = s.parts[0]
+        s_new = stream.Stream()
+        if len(part.recurse().getElementsByClass(meter.TimeSignature)) == 0:  # No time signature
+            print('no time signature')
+            for each_part in s.parts:
+                each_part.insert(0, meter.TimeSignature('4/4'))
+                s_new.append(each_part)
+
+            s_new.append(sChords)
+            s_new.write('musicxml',
+                fp=os.path.join(inputpath, fn))
+            s = converter.parse(os.path.join(inputpath, fn))
+            sChords = s.chordify(removeRedundantPitches=False)
+        for i, thisChord in enumerate(sChords.recurse().getElementsByClass('Chord')):
+            pitchClass = [0] * 12
+            only_pitch_class, pitchClass, newOnset = fill_in_pitch_class(pitchClass, thisChord.pitchClasses, thisChord,
+                                                                         'NewOnset', s, sChords, i)
+
+            meters, pitchClass = add_beat_into(pitchClass, thisChord.beatStr, '3meter', thisChord.beatStrength)
+
+            if (i == 0):
+                chorale_x = np.concatenate((chorale_x, pitchClass))
+                chorale_x_only_pitch_class = np.concatenate((chorale_x_only_pitch_class, only_pitch_class))
+                chorale_x_only_meter = np.concatenate((chorale_x_only_meter, meters))
+                chorale_x_only_newOnset = np.concatenate((chorale_x_only_newOnset, newOnset))
+            else:
+                chorale_x = np.vstack((chorale_x, pitchClass))
+                chorale_x_only_pitch_class = np.vstack((chorale_x_only_pitch_class, only_pitch_class))
+                chorale_x_only_meter = np.vstack((chorale_x_only_meter, meters))
+                chorale_x_only_newOnset = np.vstack((chorale_x_only_newOnset, newOnset))
+        file_name_x = os.path.join(encoding_path,
+                                   fn[:-4] + '.txt')
+        file_name_xx = os.path.join(encoding_path,
+                                    fn[:-4] + '_pitch_class.txt')
+        np.savetxt(file_name_x, chorale_x, fmt='%.1e')
+        np.savetxt(file_name_xx, chorale_x_only_pitch_class, fmt='%.1e')
+
+
+def predict_new_music(modelpath_NCT, modelpath_CL, inputpath):
+
+    transpose_polyphony(inputpath, inputpath)  # tranpose to 12 keys
+    encoding_path = os.path.join(inputpath, 'encodings')
+    get_input_encoding(inputpath, encoding_path)  # generate input encodings
+    xx = generate_ML_matrix(encoding_path, 1)
+    xx_no_window = generate_ML_matrix(encoding_path, 0)
+    xx_only_pitch = generate_ML_matrix(encoding_path, 0, 'Y')
+    xx_chord_tone = list(xx_only_pitch)
+    model = load_model(modelpath_NCT)  # we need to assemble x now
+    model_chord_tone = load_model(modelpath_CL)
+    predict_y = model.predict(xx)
+    for i in predict_y:  # regulate the prediction
+        for j, item in enumerate(i):
+            if (item > 0.5):
+                i[j] = 1
+            else:
+                i[j] = 0
+    for i, item in enumerate(xx_chord_tone):
+
+        NewOnset = list(xx_no_window[i][12:24])  # we need the onset sign of the vector
+        for j, item2 in enumerate(item):
+            if int(predict_y[i][j]) == 1:  # predict_y predicts NCT label for each slice
+                if int(item2) == 1:  # if the there is a current pitch class and it is predicted as a NCT
+                    xx_chord_tone[i][
+                        j] = 0  # remove this pitch class since predict_xx should be all predicted CT
+
+                    NewOnset[j] = 0
+                # else:
+                #     input('there is a NCT for a non-existing pitch class?!')
+
+        xx_chord_tone[i] = np.concatenate(
+            (xx_chord_tone[i], NewOnset))
+
+
+        xx_chord_tone[i] = np.concatenate(
+            (xx_chord_tone[i], xx_no_window[i][-3:]))  # add beat feature
+        # TODO: 3 might not be modular enough
+
+    predict_xx_chord_tone_window = adding_window_one_hot(np.asarray(xx_chord_tone), 2)
+
+
+    predict_y_chord_tone = model_chord_tone.predict_classes(predict_xx_chord_tone_window,
+                                                                verbose=0)  # TODO: we need to make this part modular so it can deal with all possible specs
+
+    fileName, numSalamiSlices = get_predict_file_name(inputpath, [], 'N')
+    length = len(fileName)
+    with open('chord_name_retrained.txt') as f:
+        chord_name = f.read().splitlines()
+    a_counter = 0
+    if not os.path.isdir(os.path.join(inputpath, 'predicted_result')):
+        os.mkdir(os.path.join(inputpath, 'predicted_result'))
+    for i in range(length):
+        num_salami_slice = numSalamiSlices[i]
+        print(fileName[i][-7:-4])
+        s = converter.parse(os.path.join(inputpath, fileName[i]))
+        sChords = s.chordify()
+        s.insert(0, sChords)
+        for j, thisChord in enumerate(sChords.recurse().getElementsByClass('Chord')):
+            thisChord.addLyric(
+                (chord_name[predict_y_chord_tone[a_counter]]))
+            a_counter += 1
+        s.write('musicxml',
+                fp=os.path.join(inputpath, 'predicted_result', fileName[i][
+                                                     :-4]) + '.xml')
+if __name__ == "__main__":
+    modelpath_NCT = os.path.join(os.getcwd(), 'ML_result', 'ISMIR2019',
+                                 '3layer300DNNwindow_size1_2training_data1timestep0ISMIR2019NCT_pitch_classpitch_class3meter_NewOnset_New_annotation_keyC__training264_39',
+                                 '3layer300DNNwindow_size1_2training_data1timestep0ISMIR2019NCT_pitch_classpitch_class3meter_NewOnset_New_annotation_keyC__training264_39_cv_1.hdf5')
+    modelpath_CL = os.path.join(os.getcwd(), 'ML_result', 'ISMIR2019',
+                                '3layer300DNNwindow_size1_2training_data1timestep0ISMIR2019NCT_pitch_classpitch_class3meter_NewOnset_New_annotation_keyC__training264_39',
+                                '3layer300DNNwindow_size1_2training_data1timestep0ISMIR2019NCT_pitch_classpitch_class3meter_NewOnset_New_annotation_keyC__training264_39_cv_1_chord_tone.hdf5')
+    # inputpath = os.path.join(os.getcwd(), 'new_muisc', 'Bach')
+    # predict_new_music(modelpath_NCT, modelpath_CL, inputpath)
+    # inputpath = os.path.join(os.getcwd(), 'new_muisc', 'Praetorius')
+    # predict_new_music(modelpath_NCT, modelpath_CL, inputpath)
+    inputpath = os.path.join(os.getcwd(), 'new_muisc', 'Schutz')
+    predict_new_music(modelpath_NCT, modelpath_CL, inputpath)
