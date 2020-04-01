08a1e5877aae0cc60d49e7256845309ae249bfe8
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2020-04-01 00:16:59 -0400

    Compare the translated chords against Sam's

--- code_snippet.py
+++ code_snippet.py
@@ -20,6 +20,78 @@ def count_pickup_measure_NO():
     # this one counts the number of pick up measures and output them
 
 
+def put_chords_into_files(sChord, f):
+    for i, thisChord in enumerate(sChord.recurse().getElementsByClass('Chord')):
+        chord_label = thisChord.lyrics[-1].text
+        chord_label = chord_label.replace('?', '').replace('!', '')
+        if i == 0 and (chord_label == ' ' or chord_label == ''):
+            input('the first chord is empty!')
+        elif chord_label == ' ' or chord_label == '':
+            if not previous_chord == ' ' or previous_chord == '':
+                print(previous_chord, file=f)
+            # else:
+            #     print('debug')
+        else:
+            print(chord_label, file=f)
+        # if i>0:
+        #     if len(previous_chord) == 0:
+        #         print('debug')
+        if chord_label != ' ' and chord_label != '':
+            previous_chord = chord_label
+
+
+def extract_chord_labels():
+    path = os.path.join(os.getcwd(), 'Bach_chorale_FB', 'FB_source', 'musicXML_master')
+    for fn in os.listdir(path):
+        if 'chordify' not in fn:
+            continue
+        if 'FB.txt' in fn or 'IR.txt' in fn:
+            continue
+        print('extracting chord labels for', fn)
+        f_FB = open(os.path.join(path, fn[:-4] + '_FB.txt'), 'w')
+        f_IR = open(os.path.join(path, fn[:-4] + '_IR.txt'), 'w')
+        s = converter.parse(os.path.join(path, fn))
+        voice_FB = s.parts[-2]
+        voice_IR = s.parts[-1]
+        put_chords_into_files(voice_IR, f_IR)
+        put_chords_into_files(voice_FB, f_FB)
+        f_FB.close()
+        f_IR.close()
+
+
+def compare_against_sam():
+    path_FB = os.path.join(os.getcwd(), 'genos-corpus', 'answer-sheets', 'bach-chorales', 'New_annotation', 'ISMIR2019', 'comparing_to_FB_translation', 'FB')
+    path_IR = os.path.join(os.getcwd(), 'genos-corpus', 'answer-sheets', 'bach-chorales', 'New_annotation', 'ISMIR2019', 'comparing_to_FB_translation', 'IR')
+    path_GT = os.path.join(os.getcwd(), 'genos-corpus', 'answer-sheets', 'bach-chorales', 'New_annotation', 'ISMIR2019', 'comparing_to_FB_translation', 'Sam')
+    a_counter = 0
+    a_counter_correct_FB = 0
+    a_counter_correct_IR = 0
+    for fn in os.listdir(path_GT):
+        f_FB = open(os.path.join(path_FB, fn[:-4] + '_FB_lyric_chordify_FB.txt'))
+        f_IR = open(os.path.join(path_IR, fn[:-4] + '_FB_lyric_chordify_IR.txt'))
+        f_GT = open(os.path.join(path_GT, fn))
+        FB_results = f_FB.readlines()
+        IR_results = f_IR.readlines()
+        GT_results = f_GT.readlines()
+        if not len(FB_results) == len(IR_results) and len(FB_results) == len(GT_results):
+            print('does not align!')
+        counter = len(IR_results)
+        counter_correct_FB = 0
+        counter_correct_IR = 0
+        for i, each_GT_label in enumerate(GT_results):
+            if each_GT_label == FB_results[i]:
+                counter_correct_FB += 1
+                a_counter_correct_FB += 1
+            if each_GT_label == IR_results[i]:
+                counter_correct_IR += 1
+                a_counter_correct_IR += 1
+        print('FB accuracy for', fn, 'is', counter_correct_FB / counter)
+        print('IR accuracy for', fn, 'is', counter_correct_IR / counter)
+        a_counter += counter
+    print('Overall FB accuracy for is', a_counter_correct_FB / a_counter)
+    print('Overall IR accuracy for is', a_counter_correct_IR / a_counter)
+
+
 def compare_chord_labels():
     input_path_array = []
     inputpath_alignment = os.path.join(os.getcwd(), 'new_music', 'New_alignment', 'predicted_result', 'original_key')
@@ -42,6 +114,6 @@ def compare_chord_labels():
 
 
 if __name__ == "__main__":
-    compare_chord_labels()
+    compare_against_sam()
     #count_pickup_measure_NO()
 
